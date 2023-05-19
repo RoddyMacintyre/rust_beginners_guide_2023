0fa9a0d48937c2fe2a0b77cf960c4b0f6b26eeaf
RoddyWiertsema <itsdigits@gmail.com>
2023-05-19 14:12:27 +0200

    Add comment types for illustration

--- section_2_project_basics/user_input/src/main.rs
+++ section_2_project_basics/user_input/src/main.rs
@@ -4,6 +4,14 @@ use std::io;
 /// This is a documentation comment, which is used for doc generation with cargo
 
 fn main() {
+    //! This is a function comment for doc generation with cargo
+
+    //! # This is a heading comment for cargo
+    //! ```
+    //! fn main()
+    //! ```
+    //! Reads user input and prints it to the console
+    //!
     // Create a mutable object through the mut directive
     let mut input = String::new();
 
