4b3df066830a05099540a4a2d40465ac0cb70781
RoddyWiertsema <itsdigits@gmail.com>
2023-06-11 23:08:33 +0200

    Section 6 // Video 31 - match

--- section_6_control_structures/src/main.rs
+++ section_6_control_structures/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use crate::Suit::{Heart, Club, Diamond, Spade};
 
 #[allow(unused_variables)]
 #[allow(unused_assignments)]
@@ -28,7 +29,42 @@ fn main() {
 
     let res = if num >= 5 {true} else {false};
     println!("{}", res);
+    println!("");
 
     // =============== MATCH ===============
+    print_choice(Heart);
+    print_choice(Club);
+    print_choice(Diamond);
+    print_choice(Spade);
 
+    country(44);
+    country(34);
+    country(125);
+    country(-15);
+}
+
+fn country(code: i32){
+    let country = match code{
+        44 => "UK",
+        34 => "Spain",
+        1..=99 => "Unknown",
+        _ => "Invalid"
+    };
+    println!("Country is {}", country);
+}
+
+enum Suit {
+    Heart,
+    Spade,
+    Club,
+    Diamond
+}
+
+fn print_choice(choice: Suit){
+    match choice{
+        Heart => {println!("\u{2665}")}
+        Spade => {println!("\u{2660}")}
+        Club => {println!("\u{2663}")}
+        Diamond => {println!("\u{2666}")}
+    }
 }
