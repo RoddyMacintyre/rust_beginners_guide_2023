94764ce39420365f4b6a18c362ed296d9e66bed6
RoddyWiertsema <itsdigits@gmail.com>
2023-08-26 14:54:11 +0200

    Section 8 // Video 40 - Overview

---
+++ section_8_traits/src/main.rs
@@ -0,0 +1,18 @@
+/*
+
+Traits expand on the flexibility we have when we use structures.
+    - Similar to OOP interfaces
+    - Defines what we can do with our structures
+
+Topics:
+    - Generics                  (different parameters, same function)
+    - dyn                       (keyword to allow dynamic types at runtime)
+    - Operator overloading
+    - Static dispatch
+    - Dynamic dispatch
+
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
