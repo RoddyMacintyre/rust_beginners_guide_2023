7578cac231e09a22853c1e995a941baff81ff295
RoddyWiertsema <itsdigits@gmail.com>
2023-08-31 15:46:50 +0200

    Section 8 // Video 41 - Traits

--- section_8_traits/src/main.rs
+++ section_8_traits/src/main.rs
@@ -13,6 +13,60 @@ Topics:
 
  */
 
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+// Define some structures
+struct RustDev{
+    awesome: bool
+}
+
+struct JavaDev{
+    awesome: bool
+}
+
+// Now create a trait
+trait Developer{
+    fn new(awesome: bool) -> Self;
+    fn language(&self) -> &str;
+    fn say_hello(&self) {println!("Hello world!")}
+
+}
+
+// Implement trait for both RustDev and JavaDev
+impl Developer for RustDev{
+    fn new(awesome: bool) -> Self {
+        RustDev{awesome: awesome}
+    }
+
+    fn language(&self) -> &str {
+        "Rust"
+    }
+
+    fn say_hello(&self) {
+        println!("println!(\"Hello world!\");");
+    }
+}
+
+impl Developer for JavaDev{
+    fn new(awesome: bool) -> Self {
+        JavaDev{awesome: awesome}
+    }
+
+    fn language(&self) -> &str {
+        "Java 1.8"
+    }
+
+    fn say_hello(&self) {
+        println!("System.out.println(\"Hello world!\")");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let r = RustDev{awesome: true};
+    let r = RustDev::new(false);
+    let j = JavaDev::new(false);
+
+    println!("{}", r.language());
+    r.say_hello();
 }
