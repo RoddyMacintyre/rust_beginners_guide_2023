abe736e63011043aa47118a5ac227c531cab3558
RoddyWiertsema <itsdigits@gmail.com>
2023-09-22 12:15:10 +0200

    Section 8 // Video 45 - Operator overloading

--- section_8_traits/src/main.rs
+++ section_8_traits/src/main.rs
@@ -138,6 +138,27 @@ impl Summable<i32> for Vec<i32>{
     }
 }
 
+// OPERATOR OVERLOADING
+// Operator is a trait, so works same as other traits for implementing
+use std::ops::Add;
+
+#[derive(Debug)]
+struct Point{
+    x: f64,
+    y: f64
+}
+
+impl Add for Point{
+    type Output = Point;
+
+    fn add(self, other: Self) -> Self::Output {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y
+        }
+    }
+}
+
 fn main() {
     // let r = RustDev{awesome: true};
     let r = RustDev::new(false);
@@ -162,4 +183,9 @@ fn main() {
     let b = vec![1.0, 2.0, 3.0];
     // Not available at this time. Need to implement first!
     // println!("sum = {}", b.sum());
+
+    let p1 = Point{x: 1.3, y: 4.6};
+    let p2 = Point{x: 3.7, y: 1.4};
+    let p3 = p1 + p2;
+    println!("{:?}", p3);   // :? = Debug formatter
 }
