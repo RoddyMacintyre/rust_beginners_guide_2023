74dd502fcb46e0e0312eee0c3697513cd27a8c06
RoddyWiertsema <itsdigits@gmail.com>
2023-09-29 15:07:42 +0200

    Section 9 // Video 50 - Borrowing

--- section_9_memory_management/src/main.rs
+++ section_9_memory_management/src/main.rs
@@ -10,6 +10,23 @@ Ownership
 Only one variable can own a piece of memory
     For primitives, copying data is cheap (know the mem size)
     For complex types, ownership is transferred (don't know size at compile time)
+
+Borrowing
+Ownership of a piece of data will be passed on, but for a temporary time (dependent on events)
+    When the variable is "finished" or destroyed, ownership will be given back to the original var
+    So:
+        Variables can borrow ownership to other pieces of memory:
+        let a = 6;
+        let b = &a;     <- give it a reference & to the value 6 in memory
+        println!("{}", *b);     <- Use * to access value in mem (this pattern is the same as c++, aka dereferencing)
+    BEWARE:
+        While borrowed, the original variable cannot use the value. So a += 2; will not work
+        until b is finished with the borrowing
+
+        Borrow has to match the mutability status of the var
+
+LIFETIMES
+Indication of how long an object will live
  */
 
 #[allow(unused_variables)]
@@ -37,4 +54,33 @@ fn main() {
     println!("{:?}", v);
     // What's the reason we can use it like this, but not like above?
     // Ownership was transferred through the function call to a complex type (in this case a vector)
+
+    // ===== BORROWING =====
+    let mut a = 6;
+    {
+        let b = &mut a;
+
+        println!("{}", *b);
+        *b += 2
+    }
+    println!("{}", a);  // Is valid as well. But cannot work with a directly under borrowing to b.
+                        // Outputs 8 because b added 2
+
+    // *b += 2;    // Is not valid. Immutable borrow occurs here.
+                // If we put the 2nd print statement, or rather access to a below this line, it will work
+                // Or scope variable b
+
+    let mut v = vec![1, 2, 3, 4, 5];
+
+    for i in &v{
+        println!("{}", i);
+        // What if we want to push an element in the vector
+        // v.push(6);
+        // Cannot borrow mutable v when it is also borrowed as immutable for the loop iterator
+        // Remember that the iterator is an object that can borrow!
+    }
+}
+
+fn get_str() -> &'static str{   // Static indicates a lifetime as long as the program.
+    "Hello"                     // Other keywords signify different lifetimes
 }
