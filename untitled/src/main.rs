ccafb0f14a81f0269893e504df69bf6c9191a335
RoddyWiertsema <itsdigits@gmail.com>
2023-10-02 22:36:37 +0200

    Section 11 // Video 60 - Channels

--- untitled/src/main.rs
+++ untitled/src/main.rs
@@ -13,11 +13,14 @@ THREADS
 
 CHANNELS
     Provides communication between threads
+    MPSC - Multiple producer, single receiver.
+            So, data comes from multiple sources with a single receiver
 
 MUTEX
     Mutual exclusion lock
  */
 
+use std::sync::mpsc;
 use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
@@ -25,7 +28,19 @@ use std::time::Duration;
 #[allow(unused_variables)]
 #[allow(unused_assignments)]
 
+const NUM_THREADS: usize = 20;  // Usize because we need data that is local to the machine for the threads
+
+fn start_thread(d: usize, tx: mpsc::Sender<usize>) {
+    thread::spawn(move || {
+        println!("Setting timer {}", d);
+        thread::sleep(Duration::from_secs(d as u64));
+        println!("Sending {}", d);
+        tx.send(d).unwrap();
+    });
+}
+
 fn main() {
+    // THREADS
     let mut threads = vec![];
 
     // Use move keyword to access variables in threads
@@ -33,7 +48,7 @@ fn main() {
         let thread_handler = thread::spawn(move ||{
             // Use sleep, but now main program finishes before the threads, blocking full execution.
             // The solution is to join all threads together before the main thread runs
-            sleep(Duration::from_millis(i * 1000));
+            sleep(Duration::from_millis(i * 100));
             println!("New thread {}", i);
         });
         threads.push(thread_handler);
@@ -44,4 +59,25 @@ fn main() {
     }
 
     println!("Main thread");
+
+    // CHANNELS
+    // let (transmitter, receiver) = mpsc::channel();
+
+    // thread::spawn(move || {
+    //     transmitter.send(42).unwrap() }
+    // );
+    //
+    // println!("Received {}", receiver.recv().unwrap());
+
+    // Now start multiple threads and have each thread send messages to the channel
+    let (transmitter, receiver) = mpsc::channel();
+
+    for i in 0..NUM_THREADS {
+        start_thread(i, transmitter.clone());   // Pass a copy of the transmitter channel
+    }
+
+    for j in receiver.iter().take(NUM_THREADS) {
+        println!("Received {}", j);
+    }
+
 }
