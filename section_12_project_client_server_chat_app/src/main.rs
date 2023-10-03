239b5057c02d7674e2b57219ed12728b44cfba94
RoddyWiertsema <itsdigits@gmail.com>
2023-10-03 20:41:46 +0200

    Section 12 // Video 62 - Project intro

---
+++ section_12_project_client_server_chat_app/src/main.rs
@@ -0,0 +1,14 @@
+/*
+Client - Server app
+    - Create server that listens to messages on an IP & port
+    - Create a client that can connect to the server
+    - Client can send message
+    - Server receives the message
+    - Server sends received message to all connected clients
+    - All clients receive the message
+    - Client can disconnect
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
