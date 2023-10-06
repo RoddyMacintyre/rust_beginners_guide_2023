32333dd142a1dfc8c559895ba0efa3e3e78a9edc
RoddyWiertsema <itsdigits@gmail.com>
2023-10-06 16:25:24 +0200

    Section 13 // Video 66 - Project intro

---
+++ section_13_snake_game/src/main.rs
@@ -0,0 +1,18 @@
+/*
+Snake game
+    - Algorithms for game mechanics
+    - GUI library
+
+    Rules:
+        - Snake can change direction, but not into the opposite of the current direction
+        - Snake can move within the box (no touching walls)
+        - Snake cannot move over its own tail
+        - Grow snake when it eats an apple
+        - Apple appears randomly on screen after eaten
+        - Restart game after 1s when game over
+ */
+
+
+fn main() {
+    println!("Hello, world!");
+}
