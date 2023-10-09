35aa06c37fdd62e9205e8c437d8c15079634ecf6
RoddyWiertsema <itsdigits@gmail.com>
2023-10-09 21:11:12 +0200

    Section 14 // Video 71 - Project info

---
+++ section_14_blockchain/src/main.rs
@@ -0,0 +1,26 @@
+/*
+Block chain project
+
+Simplified version.
+Chain of blocks with the following components
+
+    - Previous hash
+    - Timestamp
+    - Merkle
+    - Nonce
+
+Functionality to implement:
+    - Create blocks and a chain
+    - Generate genesis block
+    - Run tx on blockchain
+    - Generate new blocks
+    - Adjust difficulty
+    - Adjust reward
+
+
+*/
+
+
+fn main() {
+    println!("Hello, world!");
+}
